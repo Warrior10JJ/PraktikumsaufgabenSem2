a0d8e71ba010e2109994e1366d440a6a45268eb8
Jendrik <jej6537@thi.de>
2026-04-28 11:17:58 +0200

    main pa2 started, new folder for inclass stuff

---
+++ inclassuebung/sortetdata.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+vector<int> data;
+    int number;
+    cin >>number;
+    while (number >= 0) {
+        auto iter = data.begin();
+        while (iter != data.end() && *iter < number) {
+            iter++;
+            data.insert(iter, number);
+            cin >> number;
+        }
+        for (const auto& e: data) {
+            cout << e << endl;
+        }
+    }
+
+
+
+
+
+return 0;
+}
--- pa2/pa2_main.cpp
+++ pa2/pa2_main.cpp
@@ -6,32 +6,16 @@
 #include "ufo.h"
 
 int main() {
-    // 1. Objekte mit 'new' auf dem Heap anlegen
-    Ufo* vert = new Vertical("V-Unit-01");
-    Ufo* ball = new Ballistic("B-Unit-99", 30.0f, 60.0f);
+    //2 neue Objekte auf dem heap
+    Ufo* vert = new Vertical("V-011");
+    Ufo* ball = new Ballistic("B-12", 30,40);
 
     //vector
     std::vector<Ufo*> ufos = { vert, ball };
-
-    // konsoleneingabe
-    int index;
-    std::cout << "Welches Ufo soll fliegen? (0 für Vertical, 1 für Ballistic): ";
-    std::cin >> index;
-
-    //
-    if (index >= 0 && index < static_cast<int>(ufos.size())) {
-        std::cout << "Ufo " << ufos[index]->getId() << " startet den Flug..." << std::endl;
-
-        // flug
-
-        ufos[index]->flyToDest(100.0f, 200.0f, 50.0f, 10);
-
-        std::cout << "Flug beendet. Aktuelle Position: ";
-        std::vector<float> pos = ufos[index]->getPosition();
-        std::cout << "X: " << pos[0] << " Y: " << pos[1] << " Z: " << pos[2] << std::endl;
-    } else {
-        std::cerr << "Ungueltiger Index!" << std::endl;
-    }
+    //konsoleneingabe#
+    int zahl = 0;
+    std::cin >> zahl;
+    std::cout << zahl << std::endl;
 
     return 0;
 }
