bfc1d1186eabaf761b0954740bb6e6fd209b50fb
Jendrik <jej6537@thi.de>
2026-05-12 11:18:38 +0200

    pa4 started, udo_thread added

---
+++ pa4/ufo_thread.cpp
@@ -0,0 +1,19 @@
+#include "ufo_thread.h"
+//constructor
+UfoThread::UfoThread(Ufo *pUfo) {
+    this->flyThread = nullptr;
+    this->isFlying = false;
+
+}
+//destructor
+UfoThread::~UfoThread() {
+}
+
+void UfoThread::runner(const float x,const float y,const float height,const int speed) {
+}
+
+void UfoThread::startUfo(const float x,const float y,const float height,const int speed) {
+}
+
+bool UfoThread::getIsFlying() {
+}
---
+++ pa4/ufo_thread.h
@@ -0,0 +1,20 @@
+#ifndef PRAKTIKUM_UFO_THREAD_H
+#define PRAKTIKUM_UFO_THREAD_H
+#include <thread>
+#include "ufo.h"
+
+
+
+class UfoThread {
+private:
+    std::thread* flyThread;
+    Ufo* ufo;
+    bool isFlying;
+public:
+    UfoThread(Ufo* pUfo);
+    ~UfoThread();
+    void runner(const float x,const float y,const float height,const int speed);
+    void startUfo(const float x,const float y,const float height,const int speed);
+    bool getIsFlying();
+};
+#endif //PRAKTIKUM_UFO_THREAD_H
